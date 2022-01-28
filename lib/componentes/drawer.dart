eaadaa90ceb216146f64b8e9be1444e713ed6cc1
SamuelEngComp <samuellima810@gmail.com>
2022-01-27 22:23:48 -0300

    tela de perfil em andamento

--- lib/componentes/drawer.dart
+++ lib/componentes/drawer.dart
@@ -7,155 +7,189 @@ class MenuLateral extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
 
-      child: ListView(
-        padding: EdgeInsets.zero,
+    double altura = MediaQuery.of(context).size.height;
+    double largura = MediaQuery.of(context).size.width;
+
+
+
+    return Drawer(
+      child: Column(
         children: [
-          DrawerHeader(
-            child: Center(
-              child: Row(
-                children: [
-                  const Text('NAVEGAR'),
-                  const SizedBox(width: 30,),
-                  SizedBox(
-                    child: Image.asset('assets/imagens/logoSemFundo.png',
-                      width: 150,height: 150,),
-                  ),
-                ],
+          Container(
+            width: largura,
+            height: altura * 0.20,
+            child: DrawerHeader(
+              child: Center(
+                child: Row(
+                  children: [
+                    const Text('NAVEGAR'),
+                    const SizedBox(width: 30,),
+                    SizedBox(
+                      child: Image.asset('assets/imagens/logoSemFundo.png',
+                        width: 150,height: 150,),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
 
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('AÇÃO'),
-            hoverColor: Color.fromRGBO(189,168,252, 1),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('AÇÃO')));
-            },
+          Container(
+            width: largura,
+            height: altura * 0.70,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(189,168,252, .4),
+                    border: Border.all(
+                      color: Colors.purple,//Color(0xFFD9ACF6)
+                    ),
+                  ),
+                  child: ListTile(
+                    trailing: Icon(Icons.arrow_forward_rounded),
+                    title: const Text('AÇÃO'),
+                    hoverColor: Color.fromRGBO(189,168,252, 1),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('AÇÃO')));
+                    },
+                  ),
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('AVENTURA'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('AVENTURA')));
+                  },
+                ),
+                ListTile(
+                  focusColor: Color.fromRGBO(189,168,252, 1),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: Text('CLÁSSICOS'),
+                  textColor: Color.fromRGBO(189,168,252, .9),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('CLÁSSICOS')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('COMÉDIA'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('COMÉDIA')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('CONTOS'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('CONTOS')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('DRAMA'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('DRAMA')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('ESPIRITUAL'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('ESPIRITUAL')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('FANTASIA'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('FANTASIA')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('FICÇÃO CINETÍFICA'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('FICÇÃO CINETÍFICA')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('FICÇÃO HISTÓRICA'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('FICÇÃO HISTÓRICA')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('HUMOR'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('HUMOR')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('MISTÉRIO'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('MISTÉRIO')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('NÃO FICÇÃO'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('NÃO FICÇÃO')));
+                  },
+                ),
+                ListTile(
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  title: const Text('PARANORMAL'),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('PARANORMAL')));
+                  },
+                ),
+
+              ],
+            ),
           ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('AVENTURA'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('AVENTURA')));
-            },
-          ),
-          ListTile(
-            focusColor: Color.fromRGBO(189,168,252, 1),
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('CLÁSSICOS'),
-            textColor: Color.fromRGBO(189,168,252, .9),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('CLÁSSICOS')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('COMÉDIA'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('COMÉDIA')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('CONTOS'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('CONTOS')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('DRAMA'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('DRAMA')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('ESPIRITUAL'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('ESPIRITUAL')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('FANTASIA'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('FANTASIA')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('FICÇÃO CINETÍFICA'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('FICÇÃO CINETÍFICA')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('FICÇÃO HISTÓRICA'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('FICÇÃO HISTÓRICA')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('HUMOR'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('HUMOR')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('MISTÉRIO'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('MISTÉRIO')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('NÃO FICÇÃO'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('NÃO FICÇÃO')));
-            },
-          ),
-          ListTile(
-            trailing: Icon(Icons.arrow_forward_rounded),
-            title: const Text('PARANORMAL'),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => TelaGenerosFiltrados('PARANORMAL')));
-            },
-          ),
-          const Divider(
-            height: 20,
-            color: Color(0xFFD097FC),
-          ),
-          ListTile(
-            trailing: Icon(Icons.logout),
+
+          Container(
+            width: largura,
+            height: altura * .10,
+            child: ListTile(
+              trailing: Icon(Icons.logout),
               onTap: (){
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => TelaLogin()));
               },
-            title: const Text('SAIR'),
+              title: const Text('SAIR'),
+            ),
           ),
+
+
+
         ],
+
+
       ),
+
+
     );
   }
 }
--- lib/views/tela_perfil.dart
+++ lib/views/tela_perfil.dart
@@ -1,3 +1,6 @@
+import 'package:app_cooperativa_livros_danilo/dados/dummy_dados.dart';
+import 'package:app_cooperativa_livros_danilo/models/livro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,13 +12,158 @@ class TelaPerfil extends StatefulWidget {
 }
 
 class _TelaPerfilState extends State<TelaPerfil> {
+
+  List<Livro> livros = LIVROS;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Tela em desenvolvimento... quase lá'),
-      ),
 
+
+
+    double altura = MediaQuery.of(context).size.height;
+    double largura = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        //width: largura,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: const [
+                          Flexible(
+                            flex: 0,
+                            fit: FlexFit.loose,
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundColor: Color(0xFFD097FC),
+                              child: CircleAvatar(
+                                radius: 29,
+                                backgroundImage: AssetImage('assets/imagens/vegeta.jpeg'),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 5,),
+                          Text('Vegeta'),
+                        ],
+                      ),
+
+                      const SizedBox(width: 30,),
+
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.fromLTRB(10,0, 10, 0),
+                                label: Text('Busque seu livro aqui'),
+                                prefixIcon: Icon(Icons.search),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Chip(label: Text('*Best seller'),),
+                                SizedBox(width: 5,),
+                                Chip(label: Text('*Mistério'),),
+                                SizedBox(width: 5,),
+                                Chip(label: Text('*Lançamento'),),
+                              ],
+                            ),
+
+                          ],
+                        ),
+                      ),
+
+                    ],
+            ),
+          ),
+
+
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Text('TOP 10 LIVROS MAIS LIDOS DA SEMANA',
+                    style: TextStyle(fontWeight: FontWeight.bold),),
+                ),
+                SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: livros.length,
+                      itemBuilder: (context, index){
+                        return Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 160,
+                              width: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Image.asset(livros[index].urlImage, height: 100,),
+                                    Text(livros[index].titulo, overflow: TextOverflow.visible, textAlign: TextAlign.center,),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      }),
+                ),
+
+
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Text('SUA BIBLIOTECA',
+                    style: TextStyle(fontWeight: FontWeight.bold),),
+                ),
+                SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: livros.length,
+                      itemBuilder: (context, index){
+                        return Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 160,
+                              width: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Image.asset(livros[index].urlImage, height: 100,),
+                                    Text(livros[index].titulo, overflow: TextOverflow.visible, textAlign: TextAlign.center,),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      }),
+                ),
+
+
+
+
+
+                  ],
+                ),
+              ),
+            ),
     );
   }
 }
